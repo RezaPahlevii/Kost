201f92aeb4be45d8d7fa995f7e476eba56175be5
RezaPahlevii <rezapahlevih2o@gmail.com>
2023-02-10 15:24:20 +0700

    Routing Pages and Components/kost

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import Users from './pages/Users';
 import RumahKost from './pages/RumahKost';
 import AddUser from "./pages/AddUser";
 import EditUser from "./pages/EditUser";
+import AddRumahKost from './pages/AddRumahKost';
+import EditRumahKost from './pages/EditRumahKost';
 
 function App() {
   return (
@@ -17,6 +19,8 @@ function App() {
         <Route path="/users/add" element={<AddUser/>}/>
         <Route path="/users/edit/:id" element={<EditUser/>}/>
         <Route path="/rumah-kost" element={<RumahKost/>}/>
+        <Route path="/rumah-kost/add" element={<AddRumahKost/>}/>
+        <Route path="/rumah-kost/:id" element={<EditRumahKost/>}/>
       </Routes>
       </BrowserRouter>
     </div>
