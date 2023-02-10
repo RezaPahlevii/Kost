d43022d2b2c37e229ff05061a2192c98d1090ec2
RezaPahlevii <rezapahlevih2o@gmail.com>
2023-02-10 12:00:10 +0700

    Routing Pages and Components/user

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Dashboard from "./pages/Dashboard";
+import Users from './pages/Users';
+import RumahKost from './pages/RumahKost';
+import AddUser from "./pages/AddUser";
+import EditUser from "./pages/EditUser";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/users" element={<Users/>}/>
+        <Route path="/users/add" element={<AddUser/>}/>
+        <Route path="/users/edit/:id" element={<EditUser/>}/>
+        <Route path="/rumah-kost" element={<RumahKost/>}/>
+      </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
